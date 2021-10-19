97c164196840f216b32d2699bc75b51d5521e06f
Marcos Guibson <43528733+mguib@users.noreply.github.com>
2021-10-19 00:15:37 -0400

    Created backEnd a Noje.js NLW Heat

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,91 @@
+import { prisma } from ".prisma/client";
+import axios from "axios";
+import { sign } from "jsonwebtoken";
+import prismaClient from "../prisma";
+const { JWT_SECRET } = process.env;
+//
+// Receber o código(string)
+// Recuperar Access token do github
+// Verificar se o usuario existe no DB
+// ----SIM = gera um token
+// ------não = retorna um erro
+// Retornar token com infos do usuario logado
+//
+const { GITHUB_CLIENT_ID } = process.env;
+const { GITHUB_CLIENT_SECRET } = process.env;
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+    console.log(code);
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: GITHUB_CLIENT_ID,
+          client_secret: GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    const { avatar_url, id, login, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
+  }
+}
+
+export { AuthenticateUserService };
